212a9fab05e8b3cc884ea5fd0ce0883a26cbd584
Kaushal Raj <76280223+kaushal-create@users.noreply.github.com>
2023-11-03 07:25:13 +0530

    Time: 427 ms (86.63%) | Memory: 188.5 MB (40.11%) - LeetSync

--- 2434-design-a-number-container-system/design-a-number-container-system.cpp
+++ 2434-design-a-number-container-system/design-a-number-container-system.cpp
@@ -1,7 +1,7 @@
 class NumberContainers {
 public:
-    map<int, set<int>>mst;
-    map<int,int> mp;
+    unordered_map<int, set<int>>mst;
+    unordered_map<int,int> mp;
     NumberContainers() {}
     
     void change(int index, int number) {
